e248c47185501c0adf98c1d14dfd3fc97ba50ad3
emilydoran1 <35508852+emilydoran1@users.noreply.github.com>
2018-01-26 15:23:50 -0500

    Problem 1.4 from the book

---
+++ labs/1/PrintATable.java
@@ -0,0 +1,14 @@
+public class PrintATable
+{
+    public static void main(String[] args)
+    {
+        //this class is project 1.4 from the book
+        //this class prints a table with the value, value squared, and value cubed
+        
+        System.out.println("a \t" + "a^2 \t" + "a^3");
+        System.out.println("1 \t" + 1*1 + "\t" + 1*1*1);
+        System.out.println("2 \t" + 2*2 + "\t" + 2*2*2);
+        System.out.println("3 \t" + 3*3 + "\t" + 3*3*3);
+        System.out.println("4 \t" + 4*4 + "\t" + 4*4*4);
+    }
+}
