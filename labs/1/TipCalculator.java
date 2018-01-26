76752ae989fd70e0e9f0943c91b7d94d851a2ff1
emilydoran1 <35508852+emilydoran1@users.noreply.github.com>
2018-01-26 15:22:52 -0500

    Problem 2.5 from the book

---
+++ labs/1/TipCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class TipCalculator
+{
+    public static void main(String[] args)
+    {
+        //problem 2.5 from the book
+        //this class takes in the subtotal and gratuity amount from the user and calculates the tip and total
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the subtotal and a gratuity rate: ");
+        double subtotal = input.nextDouble();
+        double gratuityRate = input.nextDouble();
+        
+        double gratuity = subtotal * (gratuityRate/100);
+        double total = subtotal + gratuity;
+        
+        System.out.println("The gratuity is $" + gratuity + " and total is $" + total);
+    }
+}
