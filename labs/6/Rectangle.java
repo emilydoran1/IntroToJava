dc84916ab7dd1de79a3bd78a0314a2ea4054c2cd
emilydoran1 <35508852+emilydoran1@users.noreply.github.com>
2018-02-26 10:40:39 -0500

    Problem 9.1 from the book

---
+++ labs/6/Rectangle.java
@@ -0,0 +1,24 @@
+
+public class Rectangle {
+	
+	private double width;
+	private double height;
+
+	public Rectangle() {
+		width = 1;
+		height = 1;
+	}
+	
+	public Rectangle(double width, double height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	public double getArea() {
+		return width * height;
+	}
+	
+	public double getPerimeter() {
+		return (2 * width) + (2 * height);
+	}
+}
