97f7582233c11c4442fdf5031b7936163004c41a
emilydoran1 <35508852+emilydoran1@users.noreply.github.com>
2018-02-26 16:38:32 -0500

    Problem 9.7 from the book

---
+++ labs/6/Account.java
@@ -0,0 +1,66 @@
+import java.util.Date;
+
+public class Account {
+	private int id;
+	private double balance;
+	private double annualInterestRate;
+	private Date dateCreated;
+	
+	/**Default Constructor*/
+	public Account() {
+		id = 0;
+		balance = 0;
+		annualInterestRate = 0;
+		dateCreated = new Date();
+	}
+	
+	public Account(int id, double balance) {
+		this.id = id;
+		this.balance = balance;
+		dateCreated = new Date();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+
+	public void setAnnualInterestRate(double annualInterestRate) {
+		this.annualInterestRate = annualInterestRate;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+	
+	public double getMonthlyInterestRate() {
+		return getAnnualInterestRate() / 12;
+	}
+	
+	public double getMonthlyInterest() {
+		return (getMonthlyInterestRate() / 100) * balance;
+	}
+	
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+}
